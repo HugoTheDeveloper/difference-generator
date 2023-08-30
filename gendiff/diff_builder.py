756818b8b7ba5cede95b7b0e23175ac0e07535c9
bogdan <telioko@tutanota.com>
2023-08-30 11:55:36 +0300

    update diff_builder

--- gendiff/diff_builder.py
+++ gendiff/diff_builder.py
@@ -1,25 +1,18 @@
 def compare_vals(first_val, second_val):
-    status = None
-    if first_val != 'Not in dic' and second_val != 'Not in dic':
-        if first_val == second_val:
-            status = 'not changed'
-        else:
-            if isinstance(first_val, dict) and isinstance(second_val, dict):
-                status = 'nested'
-            else:
-                status = 'updated'
-
-    if first_val == 'Not in dic':
-        status = 'added'
-
-    if second_val == 'Not in dic':
-        status = 'removed'
-    return status
+    if first_val == second_val:
+        return 'not changed'
+    elif isinstance(first_val, dict) and isinstance(second_val, dict):
+        return 'nested'
+    elif first_val == 'Not in dic':
+        return 'added'
+    elif second_val == 'Not in dic':
+        return 'removed'
+    else:
+        return 'updated'
 
 
 def get_all_keys(first_dic, second_dic):
-    united_dic = first_dic.copy()
-    united_dic.update(second_dic)
+    united_dic = {**first_dic, **second_dic}
     return [key for key in united_dic.keys()]
 
 
@@ -44,7 +37,7 @@ def get_inner_diff(key, status, first_val, second_val):
 
 def build_diff(first_data, second_data):
     result = []
-    all_keys = list(sorted(get_all_keys(first_data, second_data)))
+    all_keys = sorted(get_all_keys(first_data, second_data))
     for key in all_keys:
         first_val = first_data.get(key, 'Not in dic')
         second_val = second_data.get(key, 'Not in dic')
