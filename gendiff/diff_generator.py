6dc2718899685d3bdf4537b8a5ec28a1f0f846bf
bogdan <telioko@tutanota.com>
2023-09-02 19:26:47 +0300

    update diff_generator, reader, poetry.lock, pyproject.toml, setup.cfg

--- gendiff/diff_generator.py
+++ gendiff/diff_generator.py
@@ -2,7 +2,7 @@ from gendiff.diff_builder import build_diff
 from gendiff.formatters.plain import format_plain
 from gendiff.formatters.stylish import format_stylish
 from gendiff.formatters.jsonify import format_json
-from gendiff.readers.reader import read_file_
+from gendiff.readers.reader import read_data
 
 
 OUTPUT_FORMATTER = {'json': format_json,
@@ -10,10 +10,10 @@ OUTPUT_FORMATTER = {'json': format_json,
 
 
 def generate_diff(first_path, second_path, output_format='stylish'):
-    if output_format is None:
-        output_format = 'stylish'
-    first_data = read_file_(first_path)
-    second_data = read_file_(second_path)
+    if output_format not in OUTPUT_FORMATTER:
+        raise ValueError('Incorrect output format - only stylish, json, plain.')
+    first_data = read_data(first_path)
+    second_data = read_data(second_path)
     diff = build_diff(first_data, second_data)
     formatted_diff = OUTPUT_FORMATTER[output_format](diff)
     return formatted_diff
--- gendiff/readers/reader.py
+++ gendiff/readers/reader.py
@@ -2,18 +2,25 @@ import json
 from yaml import safe_load
 import os
 
-
 FORMATTER = {'.json': json.loads, '.yaml': safe_load, '.yml': safe_load}
 
 
-def read_file_(path):
+def get_file_and_extension(path):
     path = os.path.join(path)
-    if not os.path.exists(path):
-        raise FileExistsError("This file doesn't exist or path is wrong")
     _, extension = os.path.splitext(path)
     if not FORMATTER.get(extension):
-        raise Exception("This file extension is not supported."
-                        " Only 'json', 'yaml' and 'yml' files.")
-    with open(path) as f:
-        data = FORMATTER[extension](f.read())
+        raise ValueError("This file extension is not supported."
+                         " Only 'json', 'yaml' and 'yml' files.")
+    file = open(path)
+    return file, extension
+
+
+def parse_file(file, extension):
+    parsed_data = FORMATTER[extension](file.read())
+    return parsed_data
+
+
+def read_data(path):
+    file, extension = get_file_and_extension(path)
+    data = parse_file(file, extension)
     return data
